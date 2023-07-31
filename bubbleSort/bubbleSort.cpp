977063e506c01f3519874513cdcb359b83cda6c7
Oluwadabira <adigunsamuelaso@gmail.com>
2023-07-31 19:21:57 +0100

    Creation of the BubbleSort algorithm

---
+++ bubbleSort/bubbleSort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+void bubbleSort(int array[9]);
+
+int main()
+{
+    int arr[9] = {6, 2, 1, 7, 9, 5, 3, 8, 4};
+
+    bubbleSort(arr);
+
+    for (int i = 0; i < 9; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
+// Creation of the bubbleSort algorithm
+
+void bubbleSort(int array[9])
+{
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
